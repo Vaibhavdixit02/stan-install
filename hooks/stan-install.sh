92fda9c0bc4630e724c4a610c0af974e9d8fc0f9
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2021-12-12 14:37:10 +0530

    Initial commit

---
+++ hooks/stan-install.sh
@@ -0,0 +1,4 @@
+JULIA_CMDSTAN_HOME="$HOME/cmdstan-2.26.1/"
+wget https://github.com/stan-dev/cmdstan/releases/download/v2.26.1/cmdstan-2.26.1.tar.gz
+tar -xzpf cmdstan-2.26.1.tar.gz
+make -C $JULIA_CMDSTAN_HOME build
