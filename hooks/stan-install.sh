65ffc857d991cf8e53625c4b9906bb7eacdfd89c
Vaibhav Kumar Dixit <vaibhavyashdixit@gmail.com>
2022-07-19 16:47:55 +0530

    Update stan-install.sh

--- hooks/stan-install.sh
+++ hooks/stan-install.sh
@@ -1,4 +1,13 @@
-JULIA_CMDSTAN_HOME="$HOME/cmdstan-2.26.1/"
-wget https://github.com/stan-dev/cmdstan/releases/download/v2.26.1/cmdstan-2.26.1.tar.gz
-tar -xzpf cmdstan-2.26.1.tar.gz
+JULIA_CMDSTAN_HOME="/home/runner/cmdstan-2.29.2/"
+OLDWD=`pwd`
+cd ~
+pwd
+wget https://github.com/stan-dev/cmdstan/releases/download/v2.29.2/cmdstan-2.29.2.tar.gz
+tar -xzpf cmdstan-2.29.2.tar.gz
+ls -lia .
+ls -lia ./cmdstan-2.29.2
+ls -lia ./cmdstan-2.29.2/make
+touch ./cmdstan-2.29.2/make/local
+echo "STAN_THREADS=true" > ./cmdstan-2.29.2/make/local
 make -C $JULIA_CMDSTAN_HOME build
+cd $OLDWD
